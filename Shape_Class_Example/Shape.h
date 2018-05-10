fc277100d5d06e3af7cd87e810f5ab6b96ba63da
Dom Digity <ddigity@uat.edu>
2018-05-10 12:22:39 -0700

    Add project files.

---
+++ Shape_Class_Example/Shape.h
@@ -0,0 +1,25 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+#include <string>
+#include <iostream>
+
+using namespace std;
+class Shape
+{
+public:
+	Shape();
+	~Shape();
+	string GetName();
+
+private:
+
+};
+
+Shape::Shape()
+{
+}
+
+Shape::~Shape()
+{
+}
+#endif // !SHAPE_H
